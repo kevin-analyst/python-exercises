dcad34e33e3be3a59632ffa743f4c6dc920ebe21
kevin-analyst <kberk.analyst@gmail.com>
2023-01-16 00:05:22 +0100

    Puzzle 1 executed

---
+++ exercises/python_programming_puzzles_w3/exercise_1.py
@@ -0,0 +1,27 @@
+list1 = [19, 19, 15, 5, 3, 5, 5, 2];
+list2 = [19, 15, 15, 5, 3, 3, 5, 2];
+list3 = [19, 19, 5, 5, 5, 5, 5];
+
+count1 = 2;
+count2 = 3;
+
+if(list1.count(19) == count1 and list1.count(5) >= count2):
+    print(list1);
+    print(True);
+else :
+    print(list1);
+    print(False);
+
+if(list2.count(19) == count1 and list2.count(5) >= count2):
+    print(list2);
+    print(True);
+else :
+    print(list2);
+    print(False);
+
+if(list3.count(19) == count1 and list3.count(5) >= count2):
+    print(list3);
+    print(True);
+else :
+    print(list3);
+    print(False);
